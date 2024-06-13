32eac2cf91436d6e6e6f1b9491480de54ba43605
isixe <isixe@outlook.com>
2024-06-13 20:06:13 +0800

    🧪 test(location.hash): add testing for cross-domain with location.hash & iframe

---
+++ node/location.hash/trans_outward_page_redirect/server.js
@@ -0,0 +1,51 @@
+const http = require("http");
+
+const hostname = "localhost";
+const port_root = 3000;
+const port_sub = 3001;
+
+const fs = require("fs");
+const path = require("path");
+
+// Create a index file in same directory
+const filepath = path.resolve(__dirname, "./");
+
+// First server
+http
+	.createServer(function (req, res) {
+		fs.readFile(filepath + req.url, function (err, data) {
+			if (err) {
+				res.writeHead(404, { "Content-Type": "text/plain" });
+				res.end("File not found");
+				return;
+			}
+
+			res.write(data);
+			res.end();
+		});
+	})
+	.listen(port_sub, hostname, () => {
+		console.log(`Server running at http://${hostname}:${port_sub}/sub.html`);
+	});
+
+// root server
+http
+	.createServer(function (req, res) {
+		fs.readFile(filepath + req.url, function (err, data) {
+			if (err) {
+				res.writeHead(404, { "Content-Type": "text/plain" });
+				res.end("File not found");
+				return;
+			}
+
+			res.write(data);
+			res.end();
+		});
+	})
+	.listen(port_root, hostname, () => {
+		console.log(`Server running at http://${hostname}:${port_root}/root.html`);
+	});
+
+// Start
+const childProcess = require("child_process");
+childProcess.exec(`start http://${hostname}:${port_root}/root.html`);
