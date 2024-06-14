1d7e33e429166dfc6bf66c1d7bd449b54ab39d14
isixe <isixe@outlook.com>
2024-06-14 17:14:43 +0800

    🎈 perf(server.js): refactor code for server

--- node/document.domain/diff_port/server.js
+++ node/document.domain/diff_port/server.js
@@ -1,53 +1,41 @@
 const http = require("http");
-
-const hostname = "localhost";
-const port_root = 3000;
-const port_sub = 3001;
-
 const fs = require("fs");
 const path = require("path");
 
-// Create a index file in same directory
+// Server base directory
 const filepath = path.resolve(__dirname, "./");
 
+// Read the file and respond
+const readFileAndRespond = (req, res) => {
+	fs.readFile(filepath + req.url, (err, data) => {
+		if (err) {
+			res.writeHead(404, { "Content-Type": "text/plain" });
+			res.end("File not found");
+			return;
+		}
+		// Set Origin-Agent-Cluster
+		res.setHeader("Origin-Agent-Cluster", "?0");
+		res.write(data);
+		res.end();
+	});
+};
+
+// Create server instance
+const createServer = (hostname, port, filename) => {
+	const server = http.createServer((req, res) => {
+		readFileAndRespond(req, res);
+	});
+
+	server.listen(port, hostname, () => {
+		console.log(`Server running at http://${hostname}:${port}/${filename}`);
+	});
+};
+
 // First server
-http
-	.createServer(function (req, res) {
-		fs.readFile(filepath + req.url, function (err, data) {
-			if (err) {
-				res.writeHead(404, { "Content-Type": "text/plain" });
-				res.end("File not found");
-				return;
-			}
-
-			res.setHeader("Origin-Agent-Cluster", "?0");
-			res.write(data);
-			res.end();
-		});
-	})
-	.listen(port_sub, hostname, () => {
-		console.log(`Server running at http://${hostname}:${port_sub}/sub.html`);
-	});
-
+createServer("localhost", 3000, "root.html");
 // Second server
-http
-	.createServer(function (req, res) {
-		fs.readFile(filepath + req.url, function (err, data) {
-			if (err) {
-				res.writeHead(404, { "Content-Type": "text/plain" });
-				res.end("File not found");
-				return;
-			}
-
-			res.setHeader("Origin-Agent-Cluster", "?0");
-			res.write(data);
-			res.end();
-		});
-	})
-	.listen(port_root, hostname, () => {
-		console.log(`Server running at http://${hostname}:${port_root}/root.html`);
-	});
+createServer("localhost", 3001, "sub.html");
 
 // Start
 const childProcess = require("child_process");
-childProcess.exec(`start http://${hostname}:${port_root}/root.html`);
+childProcess.exec(`start http://localhost:3000/root.html`);
