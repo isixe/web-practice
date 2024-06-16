4e74d22c162b12e1ec0ef0a5fe391f93cc7b0e0f
isixe <isixe@outlook.com>
2024-06-16 16:46:08 +0800

    🧪 test(window.postMessage): add testing for cross-domain with window.postMessage

---
+++ node/window.postMessage/diff_window/two-way_trans/server.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+// Server base directory
+const filepath = path.resolve(__dirname, "./");
+
+// Read the file and respond
+const readFileAndRespond = (req, res) => {
+	fs.readFile(filepath + req.url, (err, data) => {
+		if (err) {
+			res.writeHead(404, { "Content-Type": "text/plain" });
+			res.end("File not found");
+			return;
+		}
+
+		res.write(data);
+		res.end();
+	});
+};
+
+// Create server instance
+const createServer = (hostname, port, filename) => {
+	const server = http.createServer((req, res) => {
+		readFileAndRespond(req, res);
+	});
+
+	server.listen(port, hostname, () => {
+		console.log(`Server running at http://${hostname}:${port}/${filename}`);
+	});
+};
+
+// First server
+createServer("localhost", 3000, "root.html");
+// Second server
+createServer("localhost", 3001, "sub.html");
+
+// Start
+const childProcess = require("child_process");
+childProcess.exec(`start http://localhost:3000/root.html`);
