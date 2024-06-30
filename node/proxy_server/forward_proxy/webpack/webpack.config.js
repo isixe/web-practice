3e144295f3334b1008b3fbd5195e9462a90ff8da
isixe <isixe@outlook.com>
2024-06-30 20:19:26 +0800

    🧪 test(proxy server): add testing for cross-domain with proxy server in webpack

---
+++ node/proxy_server/forward_proxy/webpack/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+	mode: "development",
+	entry: path.join(__dirname, "/src/index.js"),
+	output: {
+		path: path.join(__dirname, "/dist"),
+		filename: "bundle.js",
+	},
+	devServer: {
+		open: true,
+		host: "127.0.0.1",
+		port: 3000,
+		static: {
+			directory: path.join(__dirname, "./dist"),
+		},
+		proxy: [
+			{
+				context: ["/api"],
+				target: "http://localhost:7000",
+			},
+		],
+	},
+};
