24193b73efa9ed10426c731424eb988df6043ac8
isixe <isixe@outlook.com>
2024-07-06 10:46:48 +0800

    🎈 perf(proxy_server): show the request result to the console

--- browser/proxy_server/vite/main.js
+++ browser/proxy_server/vite/main.js
@@ -4,4 +4,5 @@ document.querySelector("#app").innerHTML = `
 
 const xhr = new XMLHttpRequest();
 xhr.open("PUT", "/api/updateData", true);
+xhr.onload = () => console.log(xhr.responseText);
 xhr.send();
--- browser/proxy_server/webpack/src/index.js
+++ browser/proxy_server/webpack/src/index.js
@@ -1,3 +1,4 @@
 const xhr = new XMLHttpRequest();
 xhr.open("PUT", "/api/updateData", true);
+xhr.onload = () => console.log(xhr.responseText);
 xhr.send();
